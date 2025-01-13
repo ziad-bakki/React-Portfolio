df45e791f8d9ac4571dde34f71b509985b7c62e6
ziad-bakki <bakkizia@msu.edu>
2025-01-13 13:01:41 -0500

    added my portfolio title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
-
+import "./globals.css"
 export default function Home() {
   return (
-    <main 
-    style={{ display: "flex", 
-      justifyContent: "center",
-      alignItems: "center"
-}}>
-      
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={283}
-        height={64}
-        
-      />
+
+    <main>
+
+      <h1> My Porfolio</h1>
+
     </main>
+        
+
+
   );
 }
