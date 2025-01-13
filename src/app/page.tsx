3161ec3c8bf1e7bca006cfdf34567f96508b2f95
ziad-bakki <bakkizia@msu.edu>
2025-01-13 13:47:14 -0500

    experimented with react variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
-import Image from "next/image";
+"use client";
+
 import "./globals.css"
+import ReactDOM from "react-dom/client";
+
 export default function Home() {
   return (
-
     <main>
-
       <h1> My Porfolio</h1>
-
+      
     </main>
-        
-
-
+    
   );
+ 
 }
+
+const element = <h1>Test Line</h1>
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(element);
+}
+
+
