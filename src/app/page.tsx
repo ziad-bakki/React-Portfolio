ce8112f010ac21ca4c106202abd171aaebe638eb
ziad-bakki <bakkizia@msu.edu>
2025-01-15 12:51:08 -0500

    chose roboto condensed font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
 "use client";
 
 import "./globals.css"
-import ReactDOM from "react-dom/client";
+
 
 export default function Home() {
   return (
     <main>
-      <h1> My Porfolio</h1>
-      
+      <h1>Ziad Bakki</h1>
     </main>
     
   );
  
 }
 
-const element = <h1>Test Line</h1>
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(element);
-}
 
 
