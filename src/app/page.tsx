74c2c4b5296b9455acea079b0fe665e7e67902bf
ziad-bakki <bakkizia@msu.edu>
2025-01-15 15:55:09 -0500

    centered some divs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 
-import "./globals.css"
+import "./globals.css";
 
 
 export default function Home() {
   return (
     <main>
-      <h1>Ziad Bakki</h1>
+      <h1>
+        <img 
+          src="/accountcircle.svg" 
+          alt="accountcircle" 
+          style={{
+            width: "1em", // Scale image to fit text size
+            height: "1em",
+            marginRight: "0.5em", // Add space between the image and text
+            fontSize: "2em",
+            margin: "0",
+          }} 
+        />
+        Ziad Bakki
+      </h1>
+
+
     </main>
-    
   );
- 
 }
-
-
-
