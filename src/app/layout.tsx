89b262620c9db25ba9466efee209b6e4eb8552df
ziad-bakki <ziadbakki@outlook.com>
2026-03-31 23:30:00 -0400

    added subtle background effects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${jetbrains.variable}`}>
       <body className="font-sans antialiased">
+        {/* Ambient gradient glows */}
+        <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
+          <div className="absolute -top-[40%] left-1/2 h-[80vh] w-[80vh] -translate-x-1/2 rounded-full bg-blue-500/[0.03] blur-[120px]" />
+          <div className="absolute top-[60%] -left-[10%] h-[60vh] w-[60vh] rounded-full bg-purple-500/[0.03] blur-[120px]" />
+          <div className="absolute top-[30%] -right-[10%] h-[50vh] w-[50vh] rounded-full bg-teal-500/[0.02] blur-[120px]" />
+        </div>
+        {/* Dot grid overlay */}
+        <div className="bg-dot-grid pointer-events-none fixed inset-0 -z-10" />
         <Navbar />
         {children}
       </body>
